db9dabc45c5a402020501d91b5310c1eec302fe3
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2024-01-13 19:59:41 -0500

    finish header part 2

--- app/4-header-part-2/page.tsx
+++ app/4-header-part-2/page.tsx
@@ -5,40 +5,95 @@ import {
   useScroll,
   motion,
   useMotionValue,
+  useTransform,
+  useMotionTemplate,
 } from 'framer-motion';
 
-export default function Header() {
+// clamp function that takes in a number, a min, and a max value and returns a number that is guaranteed to be between the min and max values
+function clamp(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+}
+
+/**
+ * params: bounds - the number of pixels that are scrolled to return a value between 0 and 1
+ */
+function useBoundedScroll(bounds: number) {
   const { scrollY } = useScroll();
-  let height = useMotionValue(80);
+  const scrollYBounded = useMotionValue(0); // '0' is the initial value of the motion value
+  const scrollYBoundedProgress = useTransform(
+    scrollYBounded,
+    [0, bounds],
+    [0, 1],
+  );
 
   useMotionValueEvent(scrollY, 'change', (latest) => {
     const diff = latest - scrollY.getPrevious();
-    const newHeight = height.get() - diff;
-
-    height.set(Math.min(Math.max(newHeight, 50), 80));
+    const newScrollYBounded = scrollYBounded.get() + diff;
+    scrollYBounded.set(clamp(newScrollYBounded, 0, bounds));
   });
+
+  return { scrollYBounded, scrollYBoundedProgress };
+}
+
+export default function Header() {
+  const { scrollYBoundedProgress } = useBoundedScroll(400);
+  const scrollYBoundedProgressDelayed = useTransform(
+    scrollYBoundedProgress,
+    [0, 0.75, 1],
+    [0, 0, 1],
+  );
+
+  const headerHeight = useTransform(
+    scrollYBoundedProgressDelayed,
+    [0, 1],
+    [80, 50],
+  );
+  const navOpacity = useTransform(
+    scrollYBoundedProgressDelayed,
+    [0, 1],
+    [1, 0],
+  );
+  const titleScale = useTransform(
+    scrollYBoundedProgressDelayed,
+    [0, 1],
+    [1, 0.9],
+  );
+  const headerBgColorWithOpacity = useMotionTemplate`rgb(255 255 255 / ${useTransform(
+    scrollYBoundedProgressDelayed,
+    [0, 1],
+    [1, 0.1],
+  )})`;
 
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden text-slate-600">
       <div className="z-0 flex-1 overflow-y-scroll">
         <motion.header
-          style={{ height }}
-          className="fixed inset-x-0 flex h-20 bg-white shadow"
+          style={{
+            height: headerHeight,
+            backgroundColor: headerBgColorWithOpacity,
+          }}
+          className="fixed inset-x-0 flex h-20 bg-white shadow backdrop-blur-md"
         >
           <div className="mx-auto flex w-full max-w-3xl items-center justify-between px-8">
-            <p className="flex origin-left items-center text-xl font-semibold uppercase">
+            <motion.p
+              style={{ scale: titleScale }}
+              className="flex origin-left items-center text-xl font-semibold uppercase"
+            >
               <span className="-ml-1.5 inline-block -rotate-90 text-[10px] leading-[0]">
                 The
               </span>
               <span className="-ml-1 text-2xl tracking-[-.075em]">
                 Daily Bugle
               </span>
-            </p>
-            <nav className="flex space-x-4 text-xs font-medium text-slate-400">
+            </motion.p>
+            <motion.nav
+              style={{ opacity: navOpacity }}
+              className="flex space-x-4 text-xs font-medium text-slate-400"
+            >
               <a href="#">News</a>
               <a href="#">Sports</a>
               <a href="#">Culture</a>
-            </nav>
+            </motion.nav>
           </div>
         </motion.header>
 
